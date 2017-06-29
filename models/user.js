c094c1e722358ac534dcbcb179685c2f359d8f7c
Alex Close <ajclose@gmail.com>
2017-06-29 15:31:56 -0500

    Sign up page adds to database with validation

--- models/user.js
+++ models/user.js
@@ -1,9 +1,42 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Please enter a username"
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Please enter a valid email address"
+        },
+        isEmail: {
+          msg: "Please enter a valid email address"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Please enter a password"
+        },
+        len: {
+          args: [6, 20],
+          msg: "Password must be at least 6 - 20 characters long"
+        }
+      }
+    },
     bio: DataTypes.TEXT,
     image: DataTypes.STRING
   }, {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const models = require("../models")
 // const Busboy = require('busboy')
 // const path = require('path')
 // const fs = require('fs')
@@ -20,7 +21,7 @@ router.use(session({
   saveUninitialized: true
 }))
 
-router.get('/', function(req, res) {
+router.get('/signup', function(req, res) {
   res.render('signup')
 })
 
@@ -29,8 +30,37 @@ router.post('/signup', function(req, res) {
   const newUsername = req.body.username
   const newPassword = req.body.password
   const confirmPassword = req.body.confirmPassword
-  console.log(req.body);
-  res.send([newEmail, newUsername, newPassword, confirmPassword])
+  let usernameError
+  let emailError
+  let passwordError
+  const user = models.User.build({
+    username: newUsername,
+    password: newPassword,
+    email: newEmail
+  })
+  user.save().then(function(user) {
+    res.render('login', {
+      user: user,
+    })
+  }).catch(function(errors){
+    for (var i = 0; i < errors.errors.length; i++) {
+      const error = errors.errors[i]
+      if (error.path === 'username') {
+        usernameError = error.message
+      } else if (error.path === 'email') {
+        emailError = error.message
+      } else if (error.path === 'password') {
+        passwordError = error.message
+      }
+    }
+    res.render('signup', {
+      email: newEmail,
+      username: newUsername,
+      usernameError: usernameError,
+      emailError: emailError,
+      passwordError: passwordError
+    })
+})
 })
 
 module.exports = router
